3b101eb05d1d93bf8e9c38c4b16883d4dac36187
jsamuel <sp4619168@gmail.com>
2021-04-06 20:33:37 -0500

    feat: Expandir el menu de navegacion
    Se creo expand-navbar.js para expandir y contraer el menu de
    navegacion con los controles.
    Se renombro el archivo js del modal.
    Se intensifico la sombra del menu y del footer para que se
    distinguieran del fondo.

---
+++ js/expand-navbar.js
@@ -0,0 +1,8 @@
+const expandBtn = document.querySelector('#expand-navbar');
+const navbar = document.querySelector('#navbar');
+
+expandBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  navbar.classList.toggle('expanded');
+  expandBtn.classList.toggle('navbar__link--caret');
+})
